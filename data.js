42c031e71f74eeec4888fe095d7dcaa86c1e1774
Soumya <soumyakatta21@gmail.com>
2024-01-29 13:24:40 +0530

    adding data js file,which have data with it

---
+++ data.js
@@ -0,0 +1,54 @@
+export let data = [{
+    type:"1",
+    id: "s1",
+    img: "./images/shake1.jpg",
+    name:"shake1",
+    price: "$ 19.0",
+    desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+ },
+ {
+    type: "2",
+    id: "s1",
+    img: "./images/shake2.jpg",
+    name:"shake2",
+    price: "$ 13.0",
+    desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+
+ },
+ {
+  type:"3",
+  id: "b1",
+  img: "./images/break4.jpg",
+  name:"vada",
+  price: "$ 13.0",
+  desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+
+},
+{
+  type:"4",
+  id: "b1",
+  img: "./images/break5.jpg",
+  name:"bonda",
+  price: "$ 13.0",
+  desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+
+},
+{
+  type: "5",
+  id: "l1",
+  img: "./images/lunch2.jpg",
+  name:"lunch2",
+  price: "$ 13.0",
+  desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+
+},
+{
+  type: "6",
+  id: "l1",
+  img: "./images/lunch1.jpg",
+  name:"lunch1 ",
+  price: "$ 13.0",
+  desc: "As its key ingredients are rice and black gram, it is a good source of protein. A typical homemade plain dosa without oil contains about 112 calories, of which 84% is carbohydrate and 16% protein"
+
+}
+];
